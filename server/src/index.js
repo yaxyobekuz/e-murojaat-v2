5fb068b334d3c6d57ee0a5ebb3161da4faee8fa2
yaxyobekuz <pubgn9642@gmail.com>
2026-07-13 16:54:08 +0500

    chore: register users/mahalla routes and startup seeds

--- server/src/index.js
+++ server/src/index.js
@@ -6,10 +6,14 @@ import cookieParser from "cookie-parser";
 
 import { connectDb } from "./config/db.js";
 import { importLegacyJson } from "./config/importJson.js";
+import { seedUsers } from "./config/seedUsers.js";
+import { seedIndicators } from "./config/seedIndicators.js";
 import authRoutes from "./modules/auth/auth.routes.js";
 import housesRoutes from "./modules/houses/houses.routes.js";
 import residentsRoutes from "./modules/residents/residents.routes.js";
 import officialsRoutes from "./modules/officials/officials.routes.js";
+import usersRoutes from "./modules/users/users.routes.js";
+import mahallaRoutes from "./modules/mahalla/mahalla.routes.js";
 import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
@@ -24,6 +28,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/houses", housesRoutes);
 app.use("/api/residents", residentsRoutes);
 app.use("/api/officials", officialsRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/mahalla", mahallaRoutes);
 
 app.use((req, res) => res.status(404).json({ success: false, message: "Topilmadi", code: "NOT_FOUND" }));
 app.use(errorHandler);
@@ -31,6 +37,8 @@ app.use(errorHandler);
 const start = async () => {
   await connectDb();
   await importLegacyJson();
+  await seedUsers();
+  await seedIndicators();
   app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
 };
 
